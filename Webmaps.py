eae94d53a5027767a4f3e30954f550853e1c021d
vasu shah <vasushah76@gmail.com>
2019-01-11 14:31:34 -0500

    comments added to the code

--- Webmaps.py
+++ Webmaps.py
@@ -8,9 +8,6 @@ import pandas
 #Here, in location we passed latitude and longitude. The bigger the zoom_start value, the closer in you get.
 map = folium.Map(location=[38.58,-98.09],zoom_start=6)
 
-#feature group. group of features like marker
-fgv = folium.FeatureGroup(name="Valcanoes")
-
 #read data
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
@@ -27,6 +24,9 @@ def color_producer(elevation):
     else:
         return 'red'
 
+#feature group. group of features like marker
+fgv = folium.FeatureGroup(name="Valcanoes")
+
 #add marker
 for lt,ln,nm,el in zip(lat,lon,name,elev):
     fgv.add_child(folium.Marker(location=[lt,ln],popup=nm+" "+str(el)+" m",icon=folium.Icon(color=color_producer(el))))
@@ -39,8 +39,11 @@ fgp = folium.FeatureGroup(name="Population")
 fgp.add_child(folium.GeoJson(data=open("world.json",'r',encoding='utf-8-sig').read(),
                             style_function=lambda x: {'fillColor':'yellow'}))
 
+#adding feature groups to the map
 map.add_child(fgv)
 map.add_child(fgp)
+
+#adding layer control
 map.add_child(folium.LayerControl())
 
 #save map object in html file.
