189cefb7e309fb1c332d5c44ec2287c5e9cfb91f
vasu shah <vasushah76@gmail.com>
2019-01-11 13:44:25 -0500

    Read Valcanoes.txt file and add multiple marker and show appropriate popup

--- Webmaps.py
+++ Webmaps.py
@@ -1,15 +1,33 @@
 #importing folium library
 import folium
 
+#importing pandas to read and use .txt file
+import pandas
+
 #making map object.
 #Here, in location we passed latitude and longitude. The bigger the zoom_start value, the closer in you get.
-map = folium.Map(location=[42.31,-83.03],zoom_start=6)
+map = folium.Map(location=[38.58,-98.09],zoom_start=6)
 
 #feature group. group of features like marker
 fg = folium.FeatureGroup(name="MyMap")
 
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+name = list(data["NAME"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation<1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 #add marker
-fg.add_child(folium.Marker(location=[42.28,-83.02],popup="I am marker",icon=folium.Icon(color="red")))
+for lt,ln,nm,el in zip(lat,lon,name,elev):
+    fg.add_child(folium.Marker(location=[lt,ln],popup=nm+" "+str(el)+" m",icon=folium.Icon(color=color_producer(el))))
 map.add_child(fg)
 
 #save map object in html file.
